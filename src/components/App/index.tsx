918d5bbe0edaa6fddd3737a5f337ace9d43d1a1e
Vladimir <wov4@rambler.ru>
2023-08-10 14:26:06 +0200

    ВыÐРСоздал базовый компонент

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from '../../logo.svg';
-import './styles.module.scss';
+import s from './styles.module.scss';
 
-function App() {
+const App = ()  =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className={s.header}>
+        <h1 className={s.title}>Идём<span>в</span>кино</h1>
       </header>
-    </div>
+      <div>Page</div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import 'normalize.css';
+import './styles.module.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
